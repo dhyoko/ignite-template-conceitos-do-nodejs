7b8cf1df6c5a2bff2af01a7c6a40328a489386f8
Diogo Yokoyama <dih.yokoyama@gmail.com>
2023-09-20 13:32:09 -0300

    Fix check method incorrect pattern

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function checksExistsUserAccount(request, response, next) {
   const user = users.find((user) => user.username === username);
 
   if (!user) {
-    return response.status(400).json({ message: 'User not found!' });
+    return response.status(404).json({ error: 'User not found!' });
   }
 
   request.user = user;
